f74e918f5f648b0326852fd985b9bcca4d7d5565
mstvbahrain <mohdshaji117@gmail.com>
2026-06-28 20:36:45 +0300

    Fix featured video embed URL format

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   logo: '/logo.jpeg',
   youtubeUrl: 'https://www.youtube.com/@BTU411',
   instagramUrl: 'https://www.instagram.com/bahrain_theater_union/?hl=en',
-  featuredVideoEmbed: 'https://youtu.be/gaAx7hnrNF8?si=UCmJOR8NXPwZHQwm/',
+  featuredVideoEmbed: 'https://youtube.com/gaAx7hnrNF8?si=UCmJOR8NXPwZHQwm/',
   contact: {
     email: 'info@bahraintheaterunion.com',
     phone: '+973 1234 5678',
